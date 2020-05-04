2e625ab8eac67288d2c836444c06b5a4e3297d22
Richard Yen <richyen3@gmail.com>
2020-05-04 10:00:22 -0700

    add entrypoint file to automatically run migrations

---
+++ backend/api-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Wait for Postgres to start up (avoids race condition)
+for (( i=0; i<30; i++ )); do
+  [[ $( PGPASSWORD=postgres psql -h db -Atc "select ${i}" postgres postgres ) -eq ${i} ]] && break
+  echo "Still waiting after ${i} seconds"
+  sleep 1
+done
+
+# Run migrations
+python manage.py migrate
+
+# Start server
+python manage.py runserver 0.0.0.0:8000
