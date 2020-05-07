df6c19fce610e776f24b448e733104bbf6673300
Samantha Kuo <samanthakuo97@gmail.com>
2020-05-07 15:59:21 -0700

    header component v1

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+} from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css'
+
+const style = {
+  headerContainer: {
+    padding: "5px 5%"
+  },
+  
+  subContainer: {
+    display: "grid",
+    height: 30,
+    padding: 0,
+    margin: 0,
+    gridTemplateColumns: "1fr .22fr",
+    // border: "red 1px solid"
+  },
+
+  mainContainer: {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    alignItems: "center",
+    paddingTop: 15,
+    height: 65,
+    // border: "red 1px solid",
+  },
+
+  linkContainer: {
+    display: "flex",
+    alignItems: "center",
+    // border: "red 1px solid",
+  },
+
+  button: {
+    padding: "0 8px",
+    height: 40,
+    marginLeft: 20,
+    background: "none",
+    border: "gray 1px solid"
+  },
+
+  icon: {
+    background: "none"
+  }
+}
+
+const links = ["ABOUT","WHY MENTORS","PROGRAM","BLOG","CONTACT"];
+const languages = ["EN", "中文", "한국어", "日本語"];
+const socialIcons = ["circle", "circle", "circle", "youtube", "wechat"];
+
+const SubHeader = () => {
+  return (
+    <div style={style.subContainer}>
+      <div style={style.linkContainer}>
+        { languages.map(lang => <a style={{paddingRight: 10}}>{lang}</a>) }
+      </div>
+      
+      <div >
+      { socialIcons.map(icon => <Button style={{background: "none"}} icon = {icon} />) }
+      </div>
+    </div>
+  )
+}
+
+const MainHeader = () => {
+  return (
+    <div style={style.mainContainer}>
+      <h1>Logo</h1>
+      <div style={style.linkContainer}>
+        {links.map(
+          name => <a style={{padding: "0 15px"}}>{name}</a>)}
+        <Button
+          style = {style.button}
+          compact
+          content="APPLY NOW"
+        />
+      </div>
+    </div>
+  )
+}
+
+const EntireHeader = () => {
+  return(
+    <div style={style.headerContainer}>
+      <SubHeader/>
+      <MainHeader/>
+    </div>
+  )
+}
+
+export default EntireHeader;
