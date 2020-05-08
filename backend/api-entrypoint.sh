f38ab463326dbea57d72a313209e06fc734cd5a4
Richard Yen <richyen3@gmail.com>
2020-05-07 21:58:55 -0700

    separating fixtures; fix up entrypoint to test for psql return code in addition to return value

--- backend/api-entrypoint.sh
+++ backend/api-entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Wait for Postgres to start up (avoids race condition)
-for (( i=0; i<30; i++ )); do
-  [[ $( PGPASSWORD=postgres psql -h db -Atc "select ${i}" postgres postgres ) -eq ${i} ]] && break
+for (( i=0; i<60; i++ )); do
+  R=$( PGPASSWORD=postgres psql -h db -Atc "select ${i}" postgres postgres )
+  # Test both exit code and actual value returned from query
+  [[ $? -eq 0 ]] && [[ $R -eq $i ]] && break
   echo "Still waiting after ${i} seconds"
   sleep 1
 done
 
 # Run migrations
 python manage.py migrate
-python manage.py loaddata blogpost.json
+python manage.py loaddata blogpost.json blogpostcontent.json
 
 # Start server
 python manage.py runserver 0.0.0.0:8000
