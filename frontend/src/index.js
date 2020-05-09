7209b0765be0c318b7616d01da390fa206c61ffc
Samantha Kuo <samanthakuo97@gmail.com>
2020-05-09 12:08:33 -0700

    updated index.js - header component import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import EntireHeader from './components/Header';
+import EntireHeader from './components/layout/Header';
 
 ReactDOM.render(<EntireHeader />, document.getElementById('root'));
 registerServiceWorker();
