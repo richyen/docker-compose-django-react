5ae7c8b88c338429862f5711a96e2f496b157279
Albert Ke <albertjke@gmail.com>
2020-05-10 10:25:51 -0700

    chore: add superagent and api request utils

---
+++ frontend/src/agent.js
@@ -0,0 +1,40 @@
+import superagentPromise from 'superagent-promise';
+import _superagent from 'superagent';
+
+const superagent = superagentPromise(_superagent, global.Promise);
+
+// TODO: What's our API root?
+const API_ROOT = 'https://';
+
+const encode = encodeURIComponent;
+const responseBody = res => res.body;
+
+let token = null;
+const tokenPlugin = req => {
+  if (token) {
+    req.set('authorization', `Token ${token}`);
+  }
+};
+
+const requests = {
+  del: url =>
+    superagent.del(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+  get: url =>
+    superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+  put: (url, body) =>
+    superagent
+      .put(`${API_ROOT}${url}`, body)
+      .use(tokenPlugin)
+      .then(responseBody),
+  post: (url, body) =>
+    superagent
+      .post(`${API_ROOT}${url}`, body)
+      .use(tokenPlugin)
+      .then(responseBody)
+};
+
+export default {
+  setToken: _token => {
+    token = _token;
+  }
+};
