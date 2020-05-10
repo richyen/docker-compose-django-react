424b501642faf627aefbd9926cf8e22428e8c3bd
Phan Dang <philip.ddang>
2020-05-09 23:28:09 -0700

    conflicts removed v2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,18 +5,5 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-
-import {createStore} from 'redux';
-const defaultState = {checked: false};
-const reducer = function(state= defaultState, action){
-    switch (action.type) {
-        case 'TOGGLE':
-            return {...state, checked: !state.checked};
-    }
-    return state;
-};
-const store = createStore(reducer);
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
