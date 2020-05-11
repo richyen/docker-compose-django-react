734d8d1f967f98e0ff10b9926cd2c8e902395453
Richard Yen <richyen3@gmail.com>
2020-05-11 16:19:16 -0700

    rolling back pdang frontend changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import Header from './components/layout/Header';
+import './css/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Header></Header>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
