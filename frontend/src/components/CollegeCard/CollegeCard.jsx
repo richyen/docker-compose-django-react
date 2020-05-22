197e2f527c0582f5fe5245ad08f850b6eb6b4ad6
jcorea <jacorea410@gmail.com>
2020-05-21 21:48:42 -0700

    modified CollegeCard and CollegeCardList component styles to use grid instead of flexbox.

--- frontend/src/components/CollegeCard/CollegeCard.jsx
+++ frontend/src/components/CollegeCard/CollegeCard.jsx
@@ -2,27 +2,6 @@ import React from 'react';
 import Styled from 'styled-components';
 import theme from '../../styles/theme';
 
-// const StyledImage = Styled.div`
-//   width: 200px;
-//   height: 200px;
-//   background-repeat: no-repeat;
-//   background-image: url(${props => props.imgUrl});
-//   background-size: cover;
-//   position: relative;
-//   margin: 5px;
-//   border-radius: 2px;
-// `;
-
-// const StyledInfo = Styled.div`
-//   position: inherit;
-//   color: ${theme.colors.white};
-//   padding: 5%;
-//   top: 82%;
-//   text-align: left;
-//   font-family: 'Poppins';
-//   font-weight: 900;
-// `;
-
 const StyledCard = Styled.div`
     background-image: url(${props => props.imgUrl});
     width: 230px;
