f77c2fafeb46952d37ae7cfefaf8aceeb4faef5b
Jesse Woo <woojesse@gmail.com>
2020-05-27 23:26:10 -0700

    fixing up pagination silent ordering error (#38)

--- backend/api/blogpost/models.py
+++ backend/api/blogpost/models.py
@@ -2,38 +2,48 @@ from django.db import models
 from datetime import date
 
 # Create your models here.
+
+
 class Blogpost(models.Model):
-	# title = models.CharField(max_length=255, null=False)
-	# body = models.CharField(max_length=1000, null=False)
-	media_url = models.CharField(max_length=200, blank=True) #we may not use this and instead use embeddings in the
-	# text of the blogpost
-	author = models.ForeignKey(
-		'profiles.Profile', on_delete=models.CASCADE, related_name='blogpost'
-	)
-	posted_on = models.DateField(null=True)
-	last_updated = models.DateField(null=False, auto_now=True)
+    class Meta:
+        ordering = ['-id']
 
-	def __str__(self):
-		return "{} - {} - {} - {} - {}".format(self.id, self.media_url, self.author, self.posted_on, self.last_updated)
+    # title = models.CharField(max_length=255, null=False)
+    # body = models.CharField(max_length=1000, null=False)
+    # we may not use this and instead use embeddings in the
+    media_url = models.CharField(max_length=200, blank=True)
+    # text of the blogpost
+    author = models.ForeignKey(
+        'profiles.Profile', on_delete=models.CASCADE, related_name='blogpost'
+    )
+    posted_on = models.DateField(null=True)
+    last_updated = models.DateField(null=False, auto_now=True)
 
-	def create(self, validated_data):
-		return Blogpost.objects.create(**validated_data)
+    def __str__(self):
+        return "{} - {} - {} - {} - {}".format(self.id, self.media_url, self.author, self.posted_on, self.last_updated)
 
-	def update(self, instance, validated_data):
-		instance.media_url = validated_data.get("media_url", instance.language)
-		instance.author = validated_data.get("author", instance.blogpost)
-		instance.posted_on = validated_data.get("posted_on", instance.title_content)
-		instance.save()
-		return instance
+    def create(self, validated_data):
+        return Blogpost.objects.create(**validated_data)
 
-	def save(self, *args, **kwargs):
-		if self.posted_on is None:
-			self.posted_on = date.today()
-		super(Blogpost, self).save(*args, **kwargs)
+    def update(self, instance, validated_data):
+        instance.media_url = validated_data.get("media_url", instance.language)
+        instance.author = validated_data.get("author", instance.blogpost)
+        instance.posted_on = validated_data.get(
+            "posted_on", instance.title_content)
+        instance.save()
+        return instance
+
+    def save(self, *args, **kwargs):
+        if self.posted_on is None:
+            self.posted_on = date.today()
+        super(Blogpost, self).save(*args, **kwargs)
+
 
 class Tag(models.Model):
-	name = models.CharField(max_length=100)
-	blogpost = models.ManyToManyField(Blogpost, blank=True)
+    class Meta:
+        ordering = ['-id']
+    name = models.CharField(max_length=100)
+    blogpost = models.ManyToManyField(Blogpost, blank=True)
 
-	def __str__(self):
-		return "{} - {}".format(self.id, self.name)
+    def __str__(self):
+        return "{} - {}".format(self.id, self.name)
