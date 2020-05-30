b31fd64977ede0bfe8ab67f8ed676144072fe44b
pgonzales <paulo@ignidus.com>
2020-05-30 12:19:44 -0700

    one more change

--- backend/api/mentor/urls.py
+++ backend/api/mentor/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListMentorView, MentorDetailView
+from api.mentor.views import ListMentorView, MentorDetailView
 
 urlpatterns = [
    path('mentor/', ListMentorView.as_view(), name="mentors-all"),
