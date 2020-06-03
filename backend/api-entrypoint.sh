412ac4e137798c5d9cc4ab0f9f916ffdc2dd878d
Richard Yen <richyen3@gmail.com>
2020-06-03 00:41:55 -0700

    Add fixtures for mentor

--- backend/api-entrypoint.sh
+++ backend/api-entrypoint.sh
@@ -23,7 +23,7 @@ python manage.py collectstatic --no-input
 # stop anything from being loaded into the db.
 echo "Load mockup data"
 python manage.py loaddata superuser.json blogpost.json blogpost_content.json
-python manage.py loaddata school.json
+python manage.py loaddata school.json mentor.json
 python manage.py loaddata topic.json
 
 if [[ "$1" == 'test' ]]; then
