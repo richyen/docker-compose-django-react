21b5fc62a9f49324b3d05173e224501af2a31503
pgonzales <paulo@ignidus.com>
2020-06-02 21:20:22 -0700

    one more change

--- backend/api/mentor/serializers.py
+++ backend/api/mentor/serializers.py
@@ -4,4 +4,4 @@ from api.mentor.models import Mentor
 class MentorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mentor
-        fields = ("mentor_id", "picture_url", "user_id")
+        fields = ("id", "picture_url", "user_id")
