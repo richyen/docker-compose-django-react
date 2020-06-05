b42faced4326768c24a3a89eccac09f028d5b6bc
Samantha Kuo <samanthakuo97@gmail.com>
2020-06-05 12:25:45 -0700

    revert RenderBlog to master file

--- frontend/src/components/RenderBlog/RenderBlog.jsx
+++ frontend/src/components/RenderBlog/RenderBlog.jsx
@@ -5,11 +5,12 @@ import { requests } from '../../utils/agent';
 
 const test2 = `<h1>testing<h1>`;
 const BlogContainer = styled.div`
+  width: 100%;
   height: 100%;
 `;
 
-const RenderBlog = () => {
-  return <BlogContainer>{ReactHtmlParser(test2)}</BlogContainer>;
+const RenderBlog = ({ blogpostcontent_id, initial_content }) => {
+  return <BlogContainer>{ReactHtmlParser(initial_content)}</BlogContainer>;
 };
 
 export default RenderBlog;
