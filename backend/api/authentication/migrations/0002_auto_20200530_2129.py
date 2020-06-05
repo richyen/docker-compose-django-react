13fbbbe7ab47744c1ff4744f9d494d78a511c7e8
Richard Yen <richyen3@gmail.com>
2020-06-05 10:40:45 -0700

    upgrade to django 3.0.7; pylint some files

--- backend/api/authentication/migrations/0002_auto_20200530_2129.py
+++ backend/api/authentication/migrations/0002_auto_20200530_2129.py
@@ -22,7 +22,9 @@ The groups this user belongs to. A user will get all permissions granted to each
         migrations.AlterField(
             model_name='user',
             name='is_superuser',
-            field=models.BooleanField(default=False, help_text="""
-Designates that this user has all permissions without explicitly assigning them.""", verbose_name='superuser status'),
+            field=models.BooleanField(default=False,
+                                      help_text="""
+Designates that this user has all permissions without explicitly assigning them.""",
+                                      verbose_name='superuser status'),
         ),
     ]
