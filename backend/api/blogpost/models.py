7afbd8ce167e4bd0e1aef998100e402fb6866443
Douglas Chan <dtchan@eng.ucsd.edu>
2020-06-06 16:52:15 -0700

    Make blogpostcontent endpoint return both blogpost information and blogpostcontent information. Also show tag_set and topic_set as part of blogpost. (#63)

--- backend/api/blogpost/models.py
+++ backend/api/blogpost/models.py
@@ -1,7 +1,15 @@
+"""
+This module contains Blogpost, Topic, and Tag models.
+"""
 from django.db import models
 
-
 class Blogpost(models.Model):
+    """
+    This is the blogpost model. This goes hand in hand with the blogpostcontent model.
+    The reason for the split is that in the future, a blogpost will be able to be
+    associated with multiple blogpostcontent instances with different languages to allow
+    easy association of translated content.
+    """
     class Meta:
         ordering = ['-id']
 
@@ -13,12 +21,13 @@ class Blogpost(models.Model):
     is_featured = models.BooleanField(default=False)
 
     def __str__(self):
-
-        return "{} - {} - {} - {}".format(
+        return "{} - {} - {} - {} - {}".format(
             self.id,
             self.media_url,
             self.author,
-            self.slug)
+            self.slug,
+            self.is_featured
+        )
 
     def create(self, validated_data):
         return Blogpost.objects.create(**validated_data)
@@ -35,6 +44,10 @@ class Blogpost(models.Model):
 
 
 class Tag(models.Model):
+    """
+    Tags can be set by authors when they create content. Unlike topics, tags can be
+    created through the UI.
+    """
     class Meta:
         ordering = ['-id']
     name = models.CharField(max_length=100)
@@ -45,6 +58,12 @@ class Tag(models.Model):
 
 
 class Topic(models.Model):
+    """
+    The broad categories that ISMP material can be separated into. This is different
+    from tags because topics will have a small set of items that authors cannot add
+    to. This will be things like "english learning" or "adjusting to America" which
+    will get highlighted as the main topics in the blog listing page.
+    """
     class Meta:
         ordering = ['-id']
     name = models.CharField(max_length=100)
